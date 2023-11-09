f76ca2d4b2626b6e97d7164522c783391a9908a4
Francisco P <franckpedroam337@gmail.com>
2023-11-09 20:56:48 -0300

    making the page look better

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 const button = document.getElementById("advices");
 const id = document.getElementById("title");
 const text = document.getElementById("text");
-const upper = document.querySelector(".upper");
-const lower = document.querySelector(".lower");
-const InitialText = document.querySelector(".InitialText");
-
 button.addEventListener("click", function () {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((json) => {
       id.innerText = "Advice #" + json.slip.id;
       text.innerText = json.slip.advice;
-      if (!upper.classList.contains("active")) {
-        upper.classList.add("active");
-        lower.classList.add("active");
-        InitialText.classList.add("inactive");
-      }
     });
 });
